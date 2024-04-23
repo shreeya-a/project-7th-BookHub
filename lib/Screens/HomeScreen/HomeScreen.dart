92d8360ca311b4f0a970469ef7ad7358073f1097
shreeya-a <shreeyadhikari11@gmail.com>
2024-04-23 16:02:53 +0545

    book details screen UI

--- lib/Screens/HomeScreen/HomeScreen.dart
+++ lib/Screens/HomeScreen/HomeScreen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:project_7th_bookhub/Components/BookTile.dart';
 import 'package:project_7th_bookhub/Config/Colors.dart';
 // import 'package:flutter/widgets.dart';
 
 import 'package:project_7th_bookhub/Models/Data.dart';
+import 'package:project_7th_bookhub/Screens/BookDetails/BookDetails.dart';
 import 'package:project_7th_bookhub/Screens/HomeScreen/Widgets/AppBar.dart';
 import 'package:project_7th_bookhub/Screens/HomeScreen/Widgets/CategoryWidget.dart';
 import 'package:project_7th_bookhub/Screens/HomeScreen/Widgets/SearchBar.dart';
@@ -147,7 +149,9 @@ class HomeScreen extends StatelessWidget {
                             .map((e) => BookCard(
                                   title: e.title!,
                                   coverUrl: e.coverUrl!,
-                                  ontap: () {},
+                                  ontap: () {
+                                    // Get.to(BookDetails());
+                                  },
                                 ))
                             .toList(),
                       ),
