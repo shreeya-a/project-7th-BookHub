ba1d3bf4730f1b7d87187b9aa14845c82feae575
shreeya-a <shreeyadhikari11@gmail.com>
2024-04-24 18:41:12 +0545

    Add new book UI

--- lib/Components/MultiLineTextFormField.dart
+++ lib/Components/MultiLineTextFormField.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 
 class MultiLineTextField extends StatelessWidget {
   final String hintText;
+  final IconData icon;
   final TextEditingController controller;
   const MultiLineTextField({
     super.key,
     required this.hintText,
     required this.controller,
+    required this.icon,
   });
 
   @override
@@ -18,6 +20,13 @@ class MultiLineTextField extends StatelessWidget {
         fillColor: Theme.of(context).colorScheme.primary.withOpacity(0.2),
         filled: true,
         hintText: hintText,
+        prefixIcon: Icon(
+          icon
+        ),
+        border: InputBorder.none,
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Theme.of(context).colorScheme.primary), // Define border when focused
+        ),
       ),
     );
   }
---
+++ lib/Controller/BookController.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class BookController extends GetxController {
+  TextEditingController title = TextEditingController();
+  TextEditingController des = TextEditingController();
+  TextEditingController auth = TextEditingController();
+  TextEditingController aboutAuth = TextEditingController();
+  TextEditingController pages = TextEditingController();
+  TextEditingController audioLen = TextEditingController();
+  TextEditingController language = TextEditingController();
+  TextEditingController price = TextEditingController();
+}
