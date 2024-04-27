9ead233ede7e76c52fbb1b66b350f21e28b56ac2
shreeya-a <shreeyadhikari11@gmail.com>
2024-04-27 13:45:11 +0545

    uplaod image done

--- lib/Controller/BookController.dart
+++ lib/Controller/BookController.dart
@@ -1,14 +1,55 @@
 
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:uuid/uuid.dart';
 
 class BookController extends GetxController {
   TextEditingController title = TextEditingController();
-  TextEditingController des = TextEditingController();
-  TextEditingController auth = TextEditingController();
-  TextEditingController aboutAuth = TextEditingController();
+  TextEditingController description = TextEditingController();
+  TextEditingController author = TextEditingController();
+  TextEditingController aboutAuthor = TextEditingController();
   TextEditingController pages = TextEditingController();
   TextEditingController audioLen = TextEditingController();
   TextEditingController language = TextEditingController();
   TextEditingController price = TextEditingController();
+  ImagePicker imagePicker = ImagePicker();
+  final storage = FirebaseStorage.instance;
+  final db = FirebaseFirestore.instance;
+  final fAuth = FirebaseAuth.instance;
+  RxString imageUrl = "".obs;
+  RxString pdfUrl = "".obs;
+
+
+  // pick image
+  void pickImage() async {
+    final XFile? image =
+    await imagePicker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      print(image.path);
+      uploadImageToFirebase(File(image.path));
+    }
+  }
+
+// upload to firebase
+
+  void uploadImageToFirebase(File image) async {
+    var uuid = Uuid();
+    var filename = uuid.v1();
+    var storageRef = storage.ref().child("Images/$filename");
+    var response = await storageRef.putFile(image);
+    String downloadURL = await storageRef.getDownloadURL();
+    imageUrl.value = downloadURL;
+    print("Download URL: $downloadURL");
+    isImageUploading.value = false;
+  }
+
+
+
+
 }
