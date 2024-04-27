de628e09c0bd8a646503e4f9389eb5661d7e553e
shreeya-a <shreeyadhikari11@gmail.com>
2024-04-27 13:07:32 +0545

    redirect user to homepage

--- lib/Controller/SplaceController.dart
+++ lib/Controller/SplaceController.dart
@@ -1,26 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:project_7th_bookhub/Screens/HomeScreen/HomeScreen.dart';
 import 'package:project_7th_bookhub/Screens/WelcomeScreen/WelcomeScreen.dart';
 
 class SplaceController extends GetxController {
-  // final auth = FirebaseAuth.instance;
+  final auth = FirebaseAuth.instance;
 
   @override
   void onInit() {
     super.onInit();
-    // splaceController();
-    Future.delayed(const Duration(seconds: 4), () {
-      Get.offAll(() => const WelcomeScreen());
-    });
+    splaceController();
   }
 
-  // void splaceController() {
-  //   Future.delayed(const Duration(seconds: 4), () {
-  //     // if (auth.currentUser != null) {
-  //     //   Get.offAll(HomePage());
-  //     // } else {
-  //     //   Get.offAll(WelcomeScreen());
-  //     Get.offAll(() => const WelcomeScreen());
-  //     // }
-  //   });
-  // }
+  void splaceController() {
+    Future.delayed(const Duration(seconds: 6), () {
+      if (auth.currentUser != null) {
+        Get.offAll(() => const HomeScreen());
+      } else {
+        Get.offAll(() => const WelcomeScreen());
+
+      }
+
+      // }
+    });
+  }
 }
